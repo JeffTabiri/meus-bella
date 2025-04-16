0b3d851673dabf28b24997385b0fb228851862a8
Elementum <tabirijeffrey@gmail.com>
2025-04-16 11:02:18 +0200

    chore(nav): create Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-center gap-2 bg-amber-600 p-2">
-      <Link href="/">Home</Link>
-      <Link href="/about  ">About</Link>
-      <Link href="/home">Projects</Link>
+    <nav className="flex fixed top-0 left-0 w-screen justify-center gap-2 my-2">
+      <Link className="nav-button" href="/">Home</Link>
+      <Link className="nav-button" href="/about  ">About</Link>
+      <Link className="nav-button" href="/projects">Projects</Link>
+      <Link className="nav-button" href="/blog">Blog</Link>
     </nav>
   );
 }
